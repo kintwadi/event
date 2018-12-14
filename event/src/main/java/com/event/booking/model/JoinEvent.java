992bc49d5ff4fb568d6af0f3dab4d54c7eb1cb95
skoop <antmabiala@gmail.com>
2018-12-14 21:17:02 +0100

    more adds

---
+++ event/src/main/java/com/event/booking/model/JoinEvent.java
@@ -0,0 +1,150 @@
+package com.event.booking.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name ="JOIN_EVENT")
+public class JoinEvent implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	private long joinEventId;
+	private double longe; 
+	@Temporal(TemporalType.DATE)
+	private Date date;
+	private String month;
+	private String day;
+	private String hour;
+	private String minuts;
+	private String seconds;
+
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name="eventId")
+	private Event event;
+
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name="userId")
+	private User user;
+
+	public JoinEvent(){
+
+	}
+	public JoinEvent addRelationShip(Object object) {
+
+		if(object instanceof User) {
+			this.user = (User)object;
+			return this;
+		}
+
+		this.event = (Event)object;
+		return this;
+
+	}
+
+	public long getJoinEventId() {
+		return joinEventId;
+	}
+
+	public void setJoinEventId(long joinEventId) {
+		this.joinEventId = joinEventId;
+	}
+
+	public double getLonge() {
+		return longe;
+	}
+
+	public void setLonge(double longe) {
+		this.longe = longe;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getHour() {
+		return hour;
+	}
+
+	public void setHour(String hour) {
+		this.hour = hour;
+	}
+
+	public String getMinuts() {
+		return minuts;
+	}
+
+	public void setMinuts(String minuts) {
+		this.minuts = minuts;
+	}
+
+	public String getSeconds() {
+		return seconds;
+	}
+
+	public void setSeconds(String seconds) {
+		this.seconds = seconds;
+	}
+
+	public Event getEvent() {
+		return event;
+	}
+
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "JoinEvent [joinEventId=" + joinEventId + ", longe=" + longe + ", date=" + date + ", month=" + month
+				+ ", day=" + day + ", hour=" + hour + ", minuts=" + minuts + ", seconds=" + seconds + ", event=" + event
+				+ ", user=" + user + "]";
+	}
+
+
+
+}
